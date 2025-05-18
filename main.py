170f32cdfa10219c76d83e0dba531bd31ccd9465
MrSpencer9 <130394001+MrSpencer9@users.noreply.github.com>
2025-05-17 19:26:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from PIL.Image import open as load_pic, new as new_pic
 def arnolds_cat_map(path, iterations, keep_all=False, name="arnold_cat-{name}-{index}.png"):
     title = os.path.splitext(os.path.split(path)[1])[0]
     counter = 0
+
+    # Verificar si la carpeta results existe, si no, crearla
+    if not os.path.exists("results"):
+        os.makedirs("results")
+
     while counter < iterations:
         with load_pic(path) as image:
             dim = width, height = image.size
@@ -13,8 +18,10 @@ def arnolds_cat_map(path, iterations, keep_all=False, name="arnold_cat-{name}-{i
                         nx = (2 * x + y) % width
                         ny = (x + y) % height
                         canvas.putpixel((nx, height - ny - 1), image.getpixel((x, height - y - 1)))
+                
                 if counter > 0 and not keep_all:
                     os.remove(path)
+                
                 counter += 1
                 path = name.format(name=title, index=counter)
                 canvas.save(os.path.join("results", path))
@@ -22,7 +29,7 @@ def arnolds_cat_map(path, iterations, keep_all=False, name="arnold_cat-{name}-{i
     return canvas
 
 if __name__ == "__main__":
-    path = input("Ingresa la ruta a una imagen (por ejemplo, images/cat.jpg): \n\t")
+    path = input("Ingrese la ruta a una imagen (por ejemplo, images/cat.jpg): \n\t")
     while not os.path.exists(path):
         path = input("No se encontró la imagen, intenta nuevamente: \n\t")
     iterations = int(input("Ingrese el número de iteraciones: "))
